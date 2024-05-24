a5020a991fbb60429280adae344956acfa375d65
Giorgio de Rijp <rijpgr@ad.hva.nl>
2024-05-24 12:36:19 +0200

    begin product page

--- src/web/src/components/ProductPage.ts
+++ src/web/src/components/ProductPage.ts
@@ -6,6 +6,22 @@ export class ProductPage extends LitElement {
     public static styles = css`
     h1 { font-size: 20px
     }
+
+    .game-title {
+        font-size: 2em;
+        margin-left: 120px;
+        margin-top: 20px;
+    }
+    
+    .product-basic {
+        margin-left: 120px;
+        margin-top: -15px;
+        margin-right: 600px;
+        height: 450px;
+        border-radius: 2px;
+        padding: 5px;
+        background-color: #828282;
+    }
     `;
 
     protected render():TemplateResult {
@@ -13,7 +29,18 @@ export class ProductPage extends LitElement {
             <div class="navBar" id="navBar">
                 <nav-bar></nav-bar>
             </div>
-            <h1>Welcome to the Product Page</h1>
+
+            <h1 class="game-title">Titel komt hier</h1> 
+
+            <div class="product-basic">
+            </div>
+
+            <div class="aanbevelingen">
+                
+            </div>
+            
+            <div class="product-detailed"> 
+            </div>
         `;
     }
 }
