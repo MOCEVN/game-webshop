fc0f4278e262fc26bec3133f3deea90918db99d9
omar asamoah <asamoao@hva.nl>
2024-06-06 09:58:06 +0200

    notities bij de changes van gisteren samen met het ophalen van de product names

--- src/web/src/services/ShoppingcartService.ts
+++ src/web/src/services/ShoppingcartService.ts
@@ -1,5 +1,5 @@
-import {OrderItem} from "@shared/types";
-import {TokenService} from "./TokenService";
+import { OrderItem } from "@shared/types";
+import { TokenService } from "./TokenService";
 
 const headers: { "Content-Type": string } = {
     "Content-Type": "application/json",
@@ -11,24 +11,21 @@ const headers: { "Content-Type": string } = {
 // TODO Omar: Voeg commentaar toe om token-truc uit te leggen
 export class ShoppingcartService {
     private _tokenService: TokenService = new TokenService();
-
-    public async checkcart(id: number): Promise<OrderItem | undefined> {
+    public async checkcart(id: number): Promise<OrderItem[] | undefined> {
         const token: string | undefined = this._tokenService.getToken();
         if (token) {
             const response: Response = await fetch(`${viteConfiguration.API_URL}users/cart/${id}`, {
                 method: "get",
-                headers: {...headers, authorization: token},
+                headers: { ...headers, authorization: token },
             });
-
             if (!response.ok) {
                 console.error(response);
                 return undefined;
             }
-            const carts: OrderItem[] = (await response.json()) as OrderItem[];
-            return carts[0];
+
+            return (await response.json()) as OrderItem[];
         } else {
             return undefined;
         }
-    
     }
 }
