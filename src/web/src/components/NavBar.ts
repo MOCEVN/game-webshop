e972b726819faa93988cbbe9422f73d962074154
Sonny Chim <sonny.chim@hva.nl>
2024-06-12 23:51:30 +0200

    resolve merge conflict

--- src/web/src/components/NavBar.ts
+++ src/web/src/components/NavBar.ts
@@ -129,14 +129,11 @@ export class Navbar extends LitElement {
     // Login
     public async connectedCallback(): Promise<void> {
         super.connectedCallback();
-<<<<<<< HEAD
         this._hasAccess = await this._userService.requestAdminAccess();
-=======
         const urlParams: URLSearchParams = (new URL(window.location.toString())).searchParams;
         if (urlParams.has("search")){
             this._searchQuery = urlParams.get("search") as string;
         }
->>>>>>> refs/remotes/origin/main
         await this.getWelcome();
     }
 
@@ -166,14 +163,7 @@ export class Navbar extends LitElement {
             <nav class="navBar">
                 <ul>
                     <li>
-<<<<<<< HEAD
-
                         <a href="/"><img src="/assets/img/logo.png" class="logo" /></a>
-
-
-=======
-                        <a href="/"><img src="/assets/img/logo.png" class="logo" /></a>
->>>>>>> refs/remotes/origin/main
                     </li>
                     <li><a href="products.html">Producten</a></li>
                     <li class="searchBar">
@@ -184,41 +174,6 @@ export class Navbar extends LitElement {
                     </li>
 
                     <!-- Show if the user is logged in -->
-<<<<<<< HEAD
-                    ${when(
-                        this._isLoggedIn && this._isAssigned,
-                        () => html`
-                            <li>
-                                <a href="shoppingcart.html"><img src="/assets/img/cart.png" /></a>
-                            </li> 
-                            <li>
-                                <a href="#"><img src="/assets/img/heart.png" /></a>
-                            </li>
-                            <li>
-                                <a href="profile.html"><img src="/assets/img/account.png" /></a>
-                            </li>
-                              <!-- Admin panel if the admin is logged in -->
-                        ${this._hasAccess
-                            ? html`
-                                <li>
-                                    <a href="admin.html"><img src="/assets/img/admin_panel.png" /></a>
-                                </li> 
-                            `
-                            : ""}
-                            <li>
-                                <a href="#" @click=${this.clickLogoutButton}
-                                    ><img src="/assets/img/login.png" />Logout</a
-                                >
-                            </li>
-                        `,
-                        // else the user needs to login first
-                        () => html`
-                            <li>
-                                <a href="login.html" id="logIn"><img src="/assets/img/login.png" />Login</a>
-                            </li>
-                        `
-                    )}
-=======
                     <ul>
                         ${when(
                             this._isLoggedIn && this._isAssigned,
@@ -232,6 +187,14 @@ export class Navbar extends LitElement {
                                 <li>
                                     <a href="profile.html"><img src="/assets/img/account.png" /></a>
                                 </li>
+                                <!-- Admin panel if the admin is logged in -->
+                                ${this._hasAccess
+                                    ? html`
+                                        <li>
+                                            <a href="admin.html"><img src="/assets/img/admin_panel.png" /></a>
+                                        </li> 
+                                    `
+                                    : ""}
                                 <li>
                                     <a href="#" @click=${this.clickLogoutButton}
                                         ><img src="/assets/img/login.png" />Logout</a
@@ -246,7 +209,6 @@ export class Navbar extends LitElement {
                             `
                         )}
                     </ul>
->>>>>>> refs/remotes/origin/main
                 </ul>
             </nav>
         `;
