e7b4044f3ea2e463343ab8fb6a8ba3af900c4e4e
Megan <megan.sastroredjo@hva.nl>
2024-06-12 16:43:50 +0200

    ...

--- src/web/src/components/Homepage.ts
+++ src/web/src/components/Homepage.ts
@@ -69,17 +69,17 @@ export class Homepage extends LitElement {
             text-overflow: ellipsis;
             white-space: nowrap;
             width: 90%;
-<<<<<<< HEAD
+
 
             padding: 10px;
             overflow-y: auto;
             overflow-wrap: anywhere;
 
-=======
+
             padding: 10px;
             overflow-y: auto;
             overflow-wrap: anywhere;
->>>>>>> refs/remotes/origin/main
+
         }
         .price {
             display: flex;
