752f7c8ee47dd37e3b171f4c304e41faa004f867
Sonny Chim <sonny.chim@hva.nl>
2024-06-12 13:27:41 +0200

    resolve merge conflict

--- src/web/src/components/Homepage.ts
+++ src/web/src/components/Homepage.ts
@@ -69,12 +69,9 @@ export class Homepage extends LitElement {
             text-overflow: ellipsis;
             white-space: nowrap;
             width: 90%;
-<<<<<<< HEAD
-=======
             padding: 10px;
             overflow-y: auto;
             overflow-wrap: anywhere;
->>>>>>> refs/remotes/origin/main
         }
         .price {
             display: flex;
